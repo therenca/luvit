9a723a261fa54c119cc292a80edc5bba645c8f00
Therenca Mureithi <therencamureithi@gmail.com>
2023-10-09 17:56:42 +0300

    base of project; this is the shell of the project builds and renders a blank screen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:luvit/screens/shell.dart';
+
+void main() {
+  runApp(const Luvit());
+}
+
+class Luvit extends StatelessWidget {
+  const Luvit({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+			debugShowCheckedModeBanner: false,
+      title: 'Luvit',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Shell(),
+    );
+  }
+}
+
+
